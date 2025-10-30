6c6e5322614f3897bce304015ea762f51318b9c2
Umitarlez <umaborello@gmail.com>
2025-10-30 19:02:51 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+from modelo import get_class
+#1 importación de Discord 
+
+#Permisos
+intents = discord.Intents.default()
+intents.message_content = True
+
+#Crear objeto bot
+bot = commands.Bot(command_prefix='!', intents=intents)
+#Prender bot
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} se ha conectado a Discord')
+
+#Primer comando
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+        for img in ctx.message.attachments:
+            nombre_img = img.filename
+            await img.save(f"imagenes/{nombre_img}")
+            await ctx.send(get_class(
+                model_path = "./keras_model.h5",
+                labels_path ="labels.txt",
+                image_path= f"imagenes/{nombre_img}"))
+            
+    else:
+        await ctx.send("no hay imagen guardada")
+
+
+
+token = ""
+bot.run(token)
